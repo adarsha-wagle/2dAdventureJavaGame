55eb674b067d5891414abc9b68cc0e4508ce4f26
Iamnoob77 <68275507+Iamnoob77@users.noreply.github.com>
2022-10-24 09:23:35 +0545

    v12.0 New Tiles

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -24,19 +24,70 @@ This class is responsible for
     public TileManager(GamePanel gamePanel)
     {
         this.gamePanel = gamePanel;
-        tile = new Tile[6];
+        tile = new Tile[50];//number of tiles
         mapTileNum = new int[gamePanel.MAX_WORLD_COL][gamePanel.MAX_WORLD_ROW];//new int [50[[50] if not changed
         getTileImage();//loads tiles in an array
-        loadMap("/maps/world01.txt");
+        loadMap("/maps/worldV2.txt");
     }
     public void getTileImage()
     {
-            setUp(0,"grass",false);
-            setUp(1,"wall",true);
-            setUp(2,"water",true);
-            setUp(3,"earth",false);
-            setUp(4,"tree",true);
-            setUp(5,"sand",false);
+            //PLACEHOLDER
+            /*
+            We don't use the tile 0 to 9 but we set a placeholder image so we can prevent null pointer exception when scanning array
+             */
+            setUp(0,"grass00",false);
+            setUp(1,"grass00",false);
+            setUp(2,"grass00",false);
+            setUp(3,"grass00",false);
+            setUp(4,"grass00",false);
+            setUp(5,"grass00",false);
+            setUp(6,"grass00",false);
+            setUp(7,"grass00",false);
+            setUp(8,"grass00",false);
+            setUp(9,"grass00",false);
+
+            //PLACEHOLDER END
+
+            //ACTUAL TILE SETUP
+                //GRASS
+            setUp(10,"grass00",false);
+            setUp(11,"grass01",false);
+                //WATER
+            setUp(12,"water00",true);
+            setUp(13,"water01",true);
+            setUp(14,"water02",true);
+            setUp(15,"water03",true);
+            setUp(16,"water04",true);
+            setUp(17,"water05",true);
+            setUp(18,"water06",true);
+            setUp(19,"water07",true);
+            setUp(20,"water08",true);
+            setUp(21,"water09",true);
+            setUp(22,"water10",true);
+            setUp(23,"water11",true);
+            setUp(24,"water12",true);
+            setUp(25,"water13",true);
+                //ROAD
+            setUp(26,"road00",false);
+            setUp(27,"road01",false);
+            setUp(28,"road02",false);
+            setUp(29,"road03",false);
+            setUp(30,"road04",false);
+            setUp(31,"road05",false);
+            setUp(32,"road06",false);
+            setUp(33,"road07",false);
+            setUp(34,"road08",false);
+            setUp(35,"road09",false);
+            setUp(36,"road10",false);
+            setUp(37,"road11",false);
+            setUp(38,"road12",false);
+                //EARTH
+            setUp(39,"earth",false);
+                //WALL
+            setUp(40,"wall",true);
+                //TREE
+            setUp(41,"tree",true);
+
 
     }
     public void setUp(int index,String imageName,boolean collision)
